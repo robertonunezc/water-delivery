d8774cb2a37b3f142df8a610da63d2af4ae62081
Roberto Nuñez <rcorralesn@gmail.com>
2025-09-27 08:21:34 -0600

    Update entrypoint script to specify settings for database migrations and static file collection

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,10 +9,10 @@ done
 echo "Database is ready!"
 
 echo "Running database migrations..."
-python manage.py migrate --noinput
+python manage.py migrate --noinput --settings=water_delivery.settings_production
 
 echo "Collecting static files..."
-python manage.py collectstatic --noinput
+python manage.py collectstatic --noinput --settings=water_delivery.settings_production
 
 echo "Starting Gunicorn server..."
 exec gunicorn --bind 0.0.0.0:8000 water_delivery.wsgi:application
